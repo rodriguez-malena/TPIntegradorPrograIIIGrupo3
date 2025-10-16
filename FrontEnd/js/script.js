99508cc57f0c0feb17082ef72fa727b2e9a09a1e
Pereyra Sole <aishapereyrasole@gmail.com>
2025-10-16 12:41:37 -0300

    Update script.js

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,14 +1,1 @@
-const form = document.getElementById('formularioCliente');
-    const dataInput = document.getElementById('cliente');
 
-
-    form.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-
-    
-    const nombreCliente = cliente.value;
-    localStorage.setItem('miCliente', nombreCliente);
-
-    
-    window.location.href = 'index.html';
-    });
