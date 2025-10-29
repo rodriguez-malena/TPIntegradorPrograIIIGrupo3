235a8d30d0b26c05e267df44a655748d8ddf0b42
Pereyra Sole <aishapereyrasole@gmail.com>
2025-10-29 12:54:53 -0300

    inicio real

---
+++ FrontEnd/js/inicio.js
@@ -0,0 +1,14 @@
+const formularioCliente = document.getElementById('formularioCliente');
+const inputCliente = document.getElementById('cliente');
+
+formularioCliente.addEventListener('submit', function(evento) {
+    evento.preventDefault(); 
+    const nombreCliente = inputCliente.value.trim();
+    if (nombreCliente) {
+        localStorage.setItem('nombreCliente', nombreCliente);
+        window.location.href = 'index.html';
+    } else {
+        alert('Por favor ¡¡¡ingresa tu nombre para continuar!!!');
+    }
+    init();
+});
