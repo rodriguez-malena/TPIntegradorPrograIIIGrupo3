5cb5f0e79bebecc476ef8ddff90498408d78ba20
Pereyra Sole <aishapereyrasole@gmail.com>
2025-10-29 12:55:19 -0300

    Update script.js

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -182,20 +182,3 @@ function actualizarContador(){
     contadorCarrito.innerHTML = `Cantidad: ${cantidadEnCarrito} productos`;
 }
 
-
-
-const formularioCliente = document.getElementById('formularioCliente');
-const inputCliente = document.getElementById('cliente');
-
-formularioCliente.addEventListener('submit', function(evento) {
-    evento.preventDefault(); 
-    const nombreCliente = inputCliente.value.trim();
-    if (nombreCliente) {
-        localStorage.setItem('nombreCliente', nombreCliente);
-        window.location.href = 'index.html';
-    } else {
-        
-        alert('Por favor, ingresa tu nombre para continuar.');
-    }
-    init();
-});
